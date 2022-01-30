620d287396af38d51484edc31f8f37167ffce2ac
Alexey <Aly8356@gmail.com>
2022-01-30 23:43:33 +0400

    test cross domain messages

--- webpack.dev.js
+++ webpack.dev.js
@@ -90,7 +90,7 @@ module.exports = [
             historyApiFallback: true,
             host: '127.0.0.1',
             // host: '0.0.0.0',
-            port: 3000,
+            port: 3001,
             // useLocalIp: true,
             // allowedHosts: [
             //     '192.168.0.102'
